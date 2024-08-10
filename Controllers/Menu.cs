b3f129deda73524183202d3f252bd8573a9e512c
Randika Vishwajith <randikavishwajith97.rv@gmail.com>
2024-08-10 17:53:55 +0100

    Add project files.

---
+++ Controllers/Menu.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Menu.Data;
+using Menu.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Menu.Controllers
+{
+    public class Menu : Controller
+    {
+        private readonly MenuContext _context;
+        public Menu(MenuContext context) {
+         _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Dishes.ToListAsync());
+        }
+
+		public async Task<IActionResult> Details (int? id)
+		{
+			var dish = await _context.Dishes
+				.Include(di => di.DishIngredients)
+                .ThenInclude(i => i.Ingredient)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (dish == null) { 
+                return NotFound();
+            }
+            return View(dish);
+		}
+	}
+}
---
+++ Data/MenuContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Menu.Models;
+using Microsoft.Extensions.Options;
+
+namespace Menu.Data
+{
+    public class MenuContext : DbContext
+    {
+        public MenuContext(DbContextOptions<MenuContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<DishIngredient>().HasKey(di => new
+            {
+                di.DishId,
+                di.IngredientId
+            });
+
+            modelBuilder.Entity<DishIngredient>().HasOne(d => d.Dish).WithMany(di => di.DishIngredients).HasForeignKey(d => d.DishId);
+            modelBuilder.Entity<DishIngredient>().HasOne(i => i.Ingredient).WithMany(di => di.DishIngredients).HasForeignKey(i => i.IngredientId);
+
+            modelBuilder.Entity<Dish>().HasData(
+                new Dish
+                {
+                    Id = 1,
+                    Name = "Pizza",
+                    Price = 7.50,
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/9/91/Pizza-3007395.jpg",
+                });
+            modelBuilder.Entity<Ingredient>().HasData(
+                new Ingredient
+                {
+                    Id = 1,
+                    Name = "Tomato Souse"
+                },
+                 new Ingredient
+                 {
+                     Id = 2,
+                     Name = "Mozzarella"
+                 });
+            modelBuilder.Entity <DishIngredient>().HasData(
+                new DishIngredient { DishId = 1, IngredientId = 1 },
+                new DishIngredient { DishId = 1, IngredientId = 2 }
+                );
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<DishIngredient> DishIngredients { get; set; }
+    }
+}
